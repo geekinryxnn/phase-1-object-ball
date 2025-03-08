49295b18476283ddfb1c237a47a8f78c47509c88
geekinryxnn <ryankiratu67@gmail.com>
2025-03-08 13:52:17 +0300

    new changes

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,10 @@
+const { expect } = require('chai');
+
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
-
+if (typeof module !== 'undefined' && module.exports) {
+  global.expect = expect;
+}
 require("./helpers.js");
 
 describe("", () => {
